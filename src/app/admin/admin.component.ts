e1d16827681ee5d132fb3d35e53cfe97c4ef04ff
Ajay957 <ajay74.aa82@gmail.com>
2022-11-30 20:04:34 +0530

    first commit

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -7,4 +7,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
+  username=""
+  password=""
+
+  constructor(private route:Router){}
+
+  adminValues=()=>{
+    let data:any={"username":this.username,"password":this.password}
+    if(this.username=="admin" && this.password=="12345"){
+      this.route.navigate(['/viewcourse'])
+    }
+    else{
+      alert("username or password is wrong")
+    }
+  }
 }
