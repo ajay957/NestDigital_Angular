48cd301117acd6ac1f9fa4ed67d6d034aa1e8201
Ajay957 <ajay74.aa82@gmail.com>
2022-11-30 16:44:14 +0530

    first commit

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -6,11 +6,26 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
 
+  courseTitle=""
+  courseDescription=""
+  courseDuration=""
+  courseDate=""
+  courseVenue=""
+
+  readValues=()=>{
+    let data:any={"courseTitle":this.courseTitle,"courseDescription":this.courseDescription,"courseDuration":this.courseDuration,"courseDate":this.courseDate,"courseVenue":this.courseVenue}
+    console.log(data)
+  }
+
   constructor(private http:HttpClient) { }
   fetchCourse=()=>{
     return this.http.get("https://mylinkurcodesapp.herokuapp.com/getcourses")
   }
   fetchFriend=()=>{
     return this.http.get("https://dummyapifriends.herokuapp.com/adddata")
+  }
+
+  addCourse=(dataToSend:any)=>{
+    return this.http.post("https://mylinkurcodesapp.herokuapp.com/getcourses",dataToSend)
   }
 }
